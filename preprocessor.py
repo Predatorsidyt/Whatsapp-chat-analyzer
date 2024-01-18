cb8ce8df0ef954cbb3f38f29f8698b6217f2a8fe
Predatorsidyt <92681694+Predatorsidyt@users.noreply.github.com>
2024-01-18 15:45:47 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = 'r'\3/\1/\2, \4:\5 \r\nAM' if int(re.group(4)) < 12 else 'PM''
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
